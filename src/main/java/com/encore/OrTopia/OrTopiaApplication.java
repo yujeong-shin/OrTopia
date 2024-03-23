3164e20028925c1d2e2674fec2112e6dd8bfacab
yujeong-shin <vouobb@naver.com>
2024-03-23 11:41:06 +0900

    초기 개발환경 세팅

---
+++ src/main/java/com/encore/OrTopia/OrTopiaApplication.java
@@ -0,0 +1,13 @@
+package com.encore.OrTopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrTopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrTopiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/encore/OrTopia/OrTopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encore.OrTopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrTopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
