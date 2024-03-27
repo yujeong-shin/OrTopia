7bc62191c3237a0e69222d3c2dca1f48c5a67872
kwondohoon1 <kwondohoon1@naver.com>
2024-03-27 12:38:52 +0900

    [feat] Notice CRUD 구현

---
+++ notice/src/main/java/com/example/ordering_lecture/notice/dto/NoticeRequestDto.java
@@ -0,0 +1,38 @@
+package com.example.ordering_lecture.notice.dto;
+
+import com.example.ordering_lecture.notice.entity.Notice;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotNull;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NoticeRequestDto {
+    @NotNull
+    private String name;
+    private MultipartFile imagePath;
+    private boolean delYN;
+    private String startDate;
+    private String endDate;
+
+    public Notice toEntity(){
+        String fileUrl = "testing";
+        Notice notice = Notice.builder()
+                .name(this.getName())
+                .imagePath(fileUrl)
+                .startDate(this.getStartDate())
+                .endDate(this.getEndDate())
+                .build();
+        return notice;
+    }
+    public void delete() {
+        this.delYN = true;
+    }
+}
