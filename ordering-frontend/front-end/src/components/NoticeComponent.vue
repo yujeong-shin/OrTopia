a60c57eb934559bcece6ac09dd2927a1f9eb056d
kwondohoon1 <kwondohoon1@naver.com>
2024-04-01 12:41:34 +0900

    [feat] vue3, vuetify3 세팅 및 Home, Header, Footer, Notice 초기 화면 구현

---
+++ ordering-frontend/front-end/src/components/NoticeComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <h1>공지사항</h1>
+          <p>여기에 공지사항 내용을 추가하세요.</p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script setup>
+  // Vue 3 "setup" 스크립트 태그 사용
+  </script>
+  
+  <style scoped>
+  </style>
