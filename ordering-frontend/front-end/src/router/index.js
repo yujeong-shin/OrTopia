7d89534a1ae4b297d75ca006fbafd1885153b65f
Hi-Imjaeyoung <imjy2757@naver.com>
2024-04-01 16:13:28 +0900

    feat : item 상세 페이지 구현

--- ordering-frontend/front-end/src/router/index.js
+++ ordering-frontend/front-end/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeComponent from '@/components/HomeComponent.vue'
 import NoticeComponent from '@/components/NoticeComponent.vue'
+import ItemComponent from '@/components/ItemComponent.vue'
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: 'Notice',
     component: NoticeComponent,
   },
+  {
+    path:'/item',
+    name: 'ItemComponent',
+    component: ItemComponent,
+  }
 ]
 
 const router = createRouter({
