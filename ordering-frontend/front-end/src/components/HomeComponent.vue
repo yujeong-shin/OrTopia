f8e05a9d10db2bf4af7a470748e0b46f8a7ede52
Hi-Imjaeyoung <imjy2757@naver.com>
2024-04-04 15:11:19 +0900

    feat : home 화면 item 목록 정보 개발

--- ordering-frontend/front-end/src/components/HomeComponent.vue
+++ ordering-frontend/front-end/src/components/HomeComponent.vue
@@ -30,10 +30,11 @@
           <v-col cols="12">
             <v-subheader>판매중인 상품들</v-subheader>
           </v-col>
-          <v-col v-for="item in items" :key="item.id" cols="12" sm="6" md="4" lg="3">
+          <v-col v-for="item in this.itemList" :key="item.id" cols="12" sm="6" md="4" lg="3">
             <v-card>
               <v-img :src="item.thumbnail" height="200px"></v-img>
-              <v-card-title>{{ item.title }}</v-card-title>
+              <v-card-title>{{ item.name }}</v-card-title>
+              <v-card-subtitle>{{ item.price }}원</v-card-subtitle>
             </v-card>
           </v-col>
         </v-row>
@@ -41,23 +42,53 @@
     </v-main>
   </template>
   
-  <script setup>
+<script >
+  import axios from 'axios';
   import { ref } from 'vue'
   import { useRouter } from 'vue-router'
-  
-  const search = ref('')
-  const items = ref([
-    { id: 1, title: 'Item 1', thumbnail: 'https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/c817/00cc68d197d40ea6130488bd10bc2626eccb524e2b1abd0d744fe9479f2e.png' },
-    { id: 2, title: 'Item 2', thumbnail: 'https://thumbnail8.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/2f11/4e0a0a9be1cf210a6e6ac9a1b68cab60200f8210e89ba76488e39f2a352c.jpg' },
-    { id: 3, title: 'Item 3', thumbnail: 'https://thumbnail7.coupangcdn.com/thumbnails/remote/292x292ex/image/vendor_inventory/26f8/f885b892d7231bc74b825737c6615cf493e38b53cbb1c0bd17403d3d41d6.png' },
-    // 추가 상품 데이터...
-  ])
-  
-  const router = useRouter()
-  
-  const goToNotice = () => {
-    router.push('/notice')
-  }
-  
-  
-  </script>
+
+  export default {
+  created(){
+    this.getItems();
+  },
+  setup() {
+    const search = ref('')
+    const items = ref([
+      { id: 1, title: 'Item 1', thumbnail: 'https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/c817/00cc68d197d40ea6130488bd10bc2626eccb524e2b1abd0d744fe9479f2e.png' },
+      { id: 2, title: 'Item 2', thumbnail: 'https://thumbnail8.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/2f11/4e0a0a9be1cf210a6e6ac9a1b68cab60200f8210e89ba76488e39f2a352c.jpg' },
+      { id: 3, title: 'Item 3', thumbnail: 'https://thumbnail7.coupangcdn.com/thumbnails/remote/292x292ex/image/vendor_inventory/26f8/f885b892d7231bc74b825737c6615cf493e38b53cbb1c0bd17403d3d41d6.png' },
+      // 추가 상품 데이터...
+    ])
+    const router = useRouter()
+
+    const goToNotice = () => {
+      router.push('/notice')
+    }
+    return {
+      search,
+      goToNotice,
+      items,
+    }
+  },
+  data(){
+        return{
+            itemList : [],
+        }
+    },
+    methods:{
+        async getItems(){
+            // const token = localStorage.getItem('token');
+            // const refreshToken = localStorage.getItem('refreshToken');
+            // const headers = token ? { Authorization: `Bearer ${token}`, "X-Refresh-Token": `${refreshToken}` } : {};
+            try{
+                const data = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/item-service/item/items`);
+                this.itemList = data.data.result; 
+                //window.location.reload();
+            }catch(error){
+                alert(error.response.data.error_message);
+                console.log(error);
+            }
+        }
+    }
+}
+</script>
