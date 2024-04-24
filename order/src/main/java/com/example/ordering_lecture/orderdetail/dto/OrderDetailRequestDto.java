b4aa6d9a77f4ae252c7b7abf5eb2ceb9f4e61a2e
Hi-Imjaeyoung <imjy2757@naver.com>
2024-04-24 14:15:48 +0900

    feat : 주문 내역을 보여줄 떄, option의 이름과 값을 보여주도록 구현.

--- order/src/main/java/com/example/ordering_lecture/orderdetail/dto/OrderDetailRequestDto.java
+++ order/src/main/java/com/example/ordering_lecture/orderdetail/dto/OrderDetailRequestDto.java
@@ -2,9 +2,12 @@ package com.example.ordering_lecture.orderdetail.dto;
 
 import com.example.ordering_lecture.order.entity.Ordering;
 import com.example.ordering_lecture.orderdetail.entity.OrderDetail;
+import com.example.ordering_lecture.payment.dto.ItemOptionDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -14,9 +17,11 @@ public class OrderDetailRequestDto {
     private Long id;
     private int count;
     private Long sellerId;
-    public OrderDetail toEntity(Ordering ordering){
+    private List<ItemOptionDto> options;
+    public OrderDetail toEntity(Ordering ordering,String options){
         return OrderDetail.builder()
                 .ordering(ordering)
+                .options(options)
                 .itemId(this.id)
                 .sellerId(this.sellerId)
                 .quantity(this.count)
