bf98c31cb12ff694649dce6ef19c48b4ca16b839
Hi-Imjaeyoung <imjy2757@naver.com>
2024-05-02 12:14:10 +0900

    feat : git action test 18

--- item/src/main/java/com/example/ordering_lecture/ItemApplication.java
+++ item/src/main/java/com/example/ordering_lecture/ItemApplication.java
@@ -2,7 +2,7 @@ package com.example.ordering_lecture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(
@@ -12,8 +12,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
 		}
 )
-@EnableEurekaClient
+
 @EnableFeignClients
+@EnableDiscoveryClient
 public class ItemApplication {
 
 	public static void main(String[] args) {
