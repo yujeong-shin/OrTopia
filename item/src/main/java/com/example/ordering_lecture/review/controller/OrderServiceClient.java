6697a7f67fc845b0549b2205e424189e7e99c89f
Hi-Imjaeyoung <imjy2757@naver.com>
2024-05-02 14:15:43 +0900

    feat : git action test 25

--- item/src/main/java/com/example/ordering_lecture/review/controller/OrderServiceClient.java
+++ item/src/main/java/com/example/ordering_lecture/review/controller/OrderServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "order-service")
+@FeignClient(name = "ortopia-order-service", url ="http://ortopia-order-service.default.svc.cluster.local")
 public interface OrderServiceClient {
     @GetMapping("check/review/{orderDetailId}")
     OrTopiaResponse checkReview(@PathVariable(name = "orderDetailId") Long orderDetailId);
