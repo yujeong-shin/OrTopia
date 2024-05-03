6eb0d9d2c9a4583ff71aff055ea70c58986a0f50
kwondohoon1 <kwondohoon1@naver.com>
2024-05-03 15:17:18 +0900

    [refactor] 관심판매자 코드 수정정

--- ordering-frontend/front-end/src/components/LikeSellerListModal.vue
+++ ordering-frontend/front-end/src/components/LikeSellerListModal.vue
@@ -5,14 +5,13 @@
         관심판매자
       </v-card-title>
       <v-divider></v-divider>
-      <!-- 머리글 아래 선 추가 -->
       <v-card-text>
         <v-container>
           <v-row class="text-center blue-grey lighten-5">
-            <v-col cols="3" class="font-weight-bold">회사명</v-col>
-            <v-col cols="3" class="font-weight-bold">사업자등록번호</v-col>
-            <v-col cols="3" class="font-weight-bold">카테고리</v-col>
-            <v-col cols="3" class="font-weight-bold">점수</v-col>
+            <v-col cols="3">회사명</v-col>
+            <v-col cols="3">사업자등록번호</v-col>
+            <v-col cols="3">카테고리</v-col>
+            <v-col cols="3">점수</v-col>
           </v-row>
           <v-row v-for="seller in sellers" :key="seller.id" class="my-1">
             <v-col cols="3" class="subtitle-1 text-center">{{
