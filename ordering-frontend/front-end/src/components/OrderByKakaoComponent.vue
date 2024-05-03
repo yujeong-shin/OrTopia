1f7ef504b5a823754f32552c1d75c826c3528604
Hi-Imjaeyoung <imjy2757@naver.com>
2024-05-03 10:05:10 +0900

    refactor :  데이터 이름 수정

--- ordering-frontend/front-end/src/components/OrderByKakaoComponent.vue
+++ ordering-frontend/front-end/src/components/OrderByKakaoComponent.vue
@@ -49,7 +49,7 @@ export default {
           addressId: this.order.addressId,
           email: email,
           paymentMethod: "KAKAO",
-          recipient: this.order.name,
+          recipientName: this.order.name,
           phoneNumber: this.order.phoneNumber,
           orderDetailRequestDtoList: this.buyItems,
         };
