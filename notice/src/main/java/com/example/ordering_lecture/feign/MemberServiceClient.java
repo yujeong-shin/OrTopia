5a4c04b50a33c62aa2c14759d8ebcb768859dd4b
Hi-Imjaeyoung <imjy2757@naver.com>
2024-05-04 14:39:12 +0900

    refactor : 판매자의 이메일로 된 채널을 구독하게 변경.
    판매자 이벤트가 발생 할때, 자신의 채널에 데이터를 넣어주면 구독한 모든 구매자에게 메시지를 쉽게 전달이 가능함.

---
+++ notice/src/main/java/com/example/ordering_lecture/feign/MemberServiceClient.java
@@ -0,0 +1,15 @@
+package com.example.ordering_lecture.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "ortopia-member-service", url ="http://ortopia-member-service.default.svc.cluster.local")
+public interface MemberServiceClient {
+
+    // 구매자의 email로 자신이 팔로우하는 판매자의 이메일을 검색.
+    @GetMapping("/member/search/seller/emails/{email}")
+    List<String> searchEmailsBySellerId(@PathVariable("email") String email);
+}
