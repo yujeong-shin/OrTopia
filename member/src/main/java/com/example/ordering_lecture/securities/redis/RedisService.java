066fa77bbceff87229277bf3ce8199d264aa8aa2
Hi-Imjaeyoung <imjy2757@naver.com>
2024-05-07 21:43:52 +0900

    refactor : 7번 레디스 저장 상황을 위해 로그 설정.

--- member/src/main/java/com/example/ordering_lecture/securities/redis/RedisService.java
+++ member/src/main/java/com/example/ordering_lecture/securities/redis/RedisService.java
@@ -1,5 +1,6 @@
 package com.example.ordering_lecture.securities.redis;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 import java.time.Duration;
 
 @Component
+@Slf4j
 public class RedisService {
     @Qualifier("7")
     private final RedisTemplate<String,String> redisTemplate7;
@@ -19,6 +21,7 @@ public class RedisService {
     public void setValues(String key, String data) {
         ListOperations<String, String> listOps = redisTemplate7.opsForList();
         listOps.rightPush(key, data); // List의 오른쪽 끝에 데이터를 추가합니다.
+        log.info(key+"에 "+listOps.size(key)+"개의 data가 존재합니다.");
         redisTemplate7.expire(key, Duration.ofDays(7)); // 키의 TTL을 설정합니다.
     }
 }
