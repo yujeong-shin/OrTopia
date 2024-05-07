b42e773638a2d1ee07eb079ed01275a23e8eaf94
Hi-Imjaeyoung <imjy2757@naver.com>
2024-05-07 22:10:01 +0900

    refactor : 7번 레디스 저장 상황을 위해 로그 설정.

--- notice/src/main/java/com/example/ordering_lecture/redis/RedisConfig.java
+++ notice/src/main/java/com/example/ordering_lecture/redis/RedisConfig.java
@@ -54,7 +54,7 @@ public class RedisConfig {
     // 알람 캐싱 채널
     @Bean
     @Qualifier("7")
-    public RedisConnectionFactory redisConnectionFactory() {
+    public RedisConnectionFactory redisConnectionFactory7() {
         return createConnectionFactoryWith(7);
     }
     @Bean
--- notice/src/main/java/com/example/ordering_lecture/redis/RedisService.java
+++ notice/src/main/java/com/example/ordering_lecture/redis/RedisService.java
@@ -1,6 +1,7 @@
 package com.example.ordering_lecture.redis;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Component
+@Slf4j
 public class RedisService {
 
     @Qualifier("7")
@@ -18,10 +20,9 @@ public class RedisService {
         this.redisTemplate7 = redisTemplate7;
     }
 
-
-    @Transactional(readOnly = true)
     public List<String> getValues(String key) {
         ListOperations<String, String> values = redisTemplate7.opsForList();
+        log.info(key+"의 "+values.size(key)+"개의 저장 된 알람을 불러 왔습니다.");
         return values.range(key, 0, -1); // 리스트의 전체 범위를 가져옵니다.
     }
 }
