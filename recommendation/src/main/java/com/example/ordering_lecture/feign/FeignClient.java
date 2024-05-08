fc57b081bf55c74ce5d3875e607adf2fcafd6f38
Hi-Imjaeyoung <imjy2757@naver.com>
2024-05-09 02:11:58 +0900

    refactor : 추천  서버 배포.

--- recommendation/src/main/java/com/example/ordering_lecture/feign/FeignClient.java
+++ recommendation/src/main/java/com/example/ordering_lecture/feign/FeignClient.java
@@ -3,9 +3,8 @@ package com.example.ordering_lecture.feign;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@org.springframework.cloud.openfeign.FeignClient(name = "item-service", url = "localhost:8080", configuration = MyFeignClientConfig.class)
+@org.springframework.cloud.openfeign.FeignClient(name = "ortopia-item-service", url ="http://ortopia-item-service.default.svc.cluster.local",configuration = MyFeignClientConfig.class)
 public interface FeignClient {
-
     @GetMapping(value="/item-service/item/{id}/imagePath")
     String getImagePath(@PathVariable("id") Long itemId);
 }
