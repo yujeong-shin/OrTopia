d65e483a5b86c58a6a2b66a7e03b9a967bf400dc
Hi-Imjaeyoung <imjy2757@naver.com>
2024-05-08 21:42:56 +0900

    refactor : orderserver kafka 연동.

--- item/src/main/java/com/example/ordering_lecture/kafka/KafkaConfig.java
+++ item/src/main/java/com/example/ordering_lecture/kafka/KafkaConfig.java
@@ -25,13 +25,11 @@ public class KafkaConfig {
     @Value(value = "${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
     // 테스트 Topic 생성 1
-    @Bean
     public NewTopic myTopic1() {
         return new NewTopic("my_topic_1", 1, (short) 1);
     }
 
     // 테스트 Topic 생성 2
-    @Bean
     public NewTopic myTopic2() {
         return new NewTopic("my_topic_2", 1, (short) 1);
     }
