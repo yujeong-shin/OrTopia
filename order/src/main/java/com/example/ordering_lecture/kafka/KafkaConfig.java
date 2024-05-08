4fe6637ba67ddff47b9e9f9b061948edaade8bc6
Hi-Imjaeyoung <imjy2757@naver.com>
2024-05-08 21:52:34 +0900

    refactor : orderserver kafka 연동.

--- order/src/main/java/com/example/ordering_lecture/kafka/KafkaConfig.java
+++ order/src/main/java/com/example/ordering_lecture/kafka/KafkaConfig.java
@@ -35,11 +35,13 @@ public class KafkaConfig {
     // ------------------------ Publish 설정 -------------------------------------
 
     // 테스트 Topic 생성 1
+    @Bean
     public NewTopic myTopic1() {
         return new NewTopic("my_topic_1", 1, (short) 1);
     }
 
     // 테스트 Topic 생성 2
+    @Bean
     public NewTopic myTopic2() {
         return new NewTopic("my_topic_2", 1, (short) 1);
     }
