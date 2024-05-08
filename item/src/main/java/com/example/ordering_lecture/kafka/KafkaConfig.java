92ff32f34ad2a2181ea39c9ae5952a76171ed775
Hi-Imjaeyoung <imjy2757@naver.com>
2024-05-08 22:52:27 +0900

    refactor : orderserver kafka 연동.

--- item/src/main/java/com/example/ordering_lecture/kafka/KafkaConfig.java
+++ item/src/main/java/com/example/ordering_lecture/kafka/KafkaConfig.java
@@ -48,7 +48,7 @@ public class KafkaConfig {
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group1");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "foo");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(props);
