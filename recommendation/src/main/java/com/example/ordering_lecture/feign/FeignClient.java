f2b0d6b2cf532ad7597050313c9c0e3a90a4ce61
Hi-Imjaeyoung <imjy2757@naver.com>
2024-05-09 12:43:32 +0900

    refactor : 추천 서버 db 연결 변경

--- recommendation/src/main/java/com/example/ordering_lecture/feign/FeignClient.java
+++ recommendation/src/main/java/com/example/ordering_lecture/feign/FeignClient.java
@@ -3,8 +3,8 @@ package com.example.ordering_lecture.feign;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@org.springframework.cloud.openfeign.FeignClient(name = "ortopia-item-service", url ="http://ortopia-item-service.default.svc.cluster.local",configuration = MyFeignClientConfig.class)
+@org.springframework.cloud.openfeign.FeignClient(name = "ortopia-item-service", url ="http://ortopia-item-service.default.svc.cluster.local")
 public interface FeignClient {
-    @GetMapping(value="/item/{id}/imagePath")
+    @GetMapping(value="item/{id}/imagePath")
     String getImagePath(@PathVariable("id") Long itemId);
 }
